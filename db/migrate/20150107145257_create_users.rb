328dd8f5bd46604b60a41f46c57e12dc9fb8829e
Garrett Johnson <graffitisouls@gmail.com>
2015-01-07 10:05:48 -0500

    Adds user model

---
+++ db/migrate/20150107145257_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :location,
+      t.string :password_digest, null: false
+      t.string :session_id, null: false
+      t.timestamps
+    end
+  end
+end
