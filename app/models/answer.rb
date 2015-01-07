b17941a2661b147577f0583bdaa8d825992a175c
Garrett Johnson <graffitisouls@gmail.com>
2015-01-07 11:39:01 -0500

    Answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,10 @@
 class Answer < ActiveRecord::Base
+  #validations
+  validates :responder_id, :question_id, :body, :score, presence: true
+  
+  #associations
+  belongs_to(:responder, class_name: 'User',
+             foreign_key: :responder_id, primary_key: :id)
+  belongs_to(:question, class_name: 'Question',
+             foreign_key: :question_id, primary_key: :id)
 end
