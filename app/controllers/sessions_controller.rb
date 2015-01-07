a927ed07135d880360f7b47d5c12313d9e061b11
Garrett Johnson <graffitisouls@gmail.com>
2015-01-07 10:17:20 -0500

    whoops I put stuff in the wrong file. Also fills in the rails application controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,3 @@
 class SessionsController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in(@user) #TODO: sign_in in app controller
-      redirect_to root_url
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      render :new
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to root_url #TODO: this goes to the user show page probs
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      render :edit
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:username, :password, :location)
-  end
+  
 end
