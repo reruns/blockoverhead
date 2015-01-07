ca545bf78ab8138fadc51b7b506fd247f8850222
Garrett Johnson <graffitisouls@gmail.com>
2015-01-07 11:06:03 -0500

    Adds some simple pages for questions, maybe now we can sort of test things?

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,6 +13,14 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "sessions#new"
 
-  resources :users, except: [:destroy]
+  resources :users, except: [:destroy, :index]
   resource :session, only: [:new, :create, :destroy]
-  resources :questions, only: [:new, :create]
+  resources :questions, only: [:new, :create, :index]
 end
