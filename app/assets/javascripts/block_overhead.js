a349c0343e5c70b67dd73a569c48d5ce03111981
Garrett <graffitisouls@gmail.com>
2015-01-09 07:22:10 -0500

    Backbone setup

---
+++ app/assets/javascripts/block_overhead.js
@@ -0,0 +1,13 @@
+window.BlockOverhead = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  BlockOverhead.initialize();
+});
